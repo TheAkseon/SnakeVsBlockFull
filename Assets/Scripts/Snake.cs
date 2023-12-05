38ad6526f330e26dbf83ecbd851f7e0d62389b8c
Kirill <abqwerty390@gmail.com>
2023-12-05 13:03:55 +0300

    added snake movement

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,17 +1,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SnakeHead), typeof(TailGenerator))]
+[RequireComponent(typeof(TailGenerator))]
 public class Snake : MonoBehaviour
 {
-    private SnakeHead _head;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _tailSpringiness;
+    [SerializeField] private SnakeHead _head;
+
     private List<Segment> _tail;
     private TailGenerator _tailGenerator;
 
     private void Awake()
     {
-        _head = GetComponent<SnakeHead>();
         _tailGenerator = GetComponent<TailGenerator>();
         _tail = _tailGenerator.Generate();
+    }
+
+    private void FixedUpdate()
+    {
+        Move(_head.transform.position + _head.transform.up * _speed * Time.fixedDeltaTime);
+    }
+
+    private void Move(Vector3 nextPosition)
+    {
+        Vector3 previousPosition = _head.transform.position;
+
+        foreach (Segment segment in _tail)
+        {
+            Vector3 tempPosition = segment.transform.position;
+            segment.transform.position = Vector2.Lerp(segment.transform.position, previousPosition, 
+                _tailSpringiness * Time.deltaTime);
+            previousPosition = tempPosition;
+        }
+
+        _head.Move(nextPosition);
     }
 }
