318f57ad2059907c8694e827622a6f1a622cd599
Kirill <abqwerty390@gmail.com>
2023-12-05 11:50:19 +0300

    added tail generation

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,6 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SnakeHead), typeof(TailGenerator))]
 public class Snake : MonoBehaviour
 {
-    
+    private SnakeHead _head;
+    private List<Segment> _tail;
+    private TailGenerator _tailGenerator;
+
+    private void Awake()
+    {
+        _head = GetComponent<SnakeHead>();
+        _tailGenerator = GetComponent<TailGenerator>();
+        _tail = _tailGenerator.Generate();
+    }
 }
